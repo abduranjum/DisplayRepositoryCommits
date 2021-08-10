71860ac0c4326af62d4a2b34c93b2b462b2749f6
Abdurrahman Mubeen Ali <abdur.anjum@gmail.com>
2021-08-10 18:16:57 -0500

    Created custom self sizing cells.

---
+++ DisplayRepositoryCommits/Views/Cells/CommitTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CommitTableViewCell.swift
+//  DisplayRepositoryCommits
+//
+//  Created by Abdurrahman Ali on 8/10/21.
+//
+
+import Foundation
+import UIKit
+
+class CommitTableViewCell: UITableViewCell {
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var hashLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+}
